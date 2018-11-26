735001b60ff7602f6e808fa4e2dfd40964cc081f
donrestarone <shashikejayatunge@gmail.com>
2018-11-25 22:42:50 -0500

    showing occurances

--- menrva/app/controllers/comparisons_controller.rb
+++ menrva/app/controllers/comparisons_controller.rb
@@ -11,7 +11,8 @@ class ComparisonsController < ApplicationController
     if comparison
       # analyze
       @occurances = Analyzer.occurances_of_words(comparison.body, false)
-      @occurances_with_exceptions = Analyzer.occurances_of_words(comparison.body, true)
+      # debug- > remove duplicates from this
+      @occurances_with_exceptions = Analyzer.occurances_of_words(comparison.body, true).uniq
       @summary = Analyzer.summarize(comparison.body, 4)
       respond_to do |format|
         format.js {}
